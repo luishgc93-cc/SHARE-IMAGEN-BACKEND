22f1da017f055c0b2401944d7ea468103982965a
Luis H <luis.gonzales@oatobservatorio.com>
2022-08-02 23:26:05 +0200

    añado ruta home

--- index.js
+++ index.js
@@ -34,6 +34,9 @@ app.listen(PORT, () => {
   console.log(`server corriendo en puerto : ${PORT}`)
 })
 
+app.get('/', (request, response) => {
+  response.send('<h2>home</h2>')
+})
 
 app.post('/a', (request, response, next) => {
   console.log('recibiendo imagen...');
