e4c687236cd9b79dae4a29b2366d4094f6cb1a10
Luis H <luis.gonzales@oatobservatorio.com>
2022-08-03 00:18:01 +0200

    cors

--- index.js
+++ index.js
@@ -13,13 +13,6 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(fileUpload())
 app.use(express.static('files'))
 
-const corsOption = {
-  origin: ['http://localhost:3000'],
-};
-
-app.use(cors(corsOption));
-// Configurar cabeceras y cors
-
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
